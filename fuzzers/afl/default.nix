9e3aec9eea4a5937ba159be3bf7a3dee82c31cdc
Chibin Zhang <chibinzhang@hotmail.com>
2022-08-02 04:45:07 +0000

    feat: Get AFL instrumented libpng building

--- fuzzers/afl/default.nix
+++ fuzzers/afl/default.nix
@@ -1,26 +1,33 @@
-{ pkgs
-,
-}:
+{ pkgs ? import <nixpkgs> {} }:
 
 let
   llvmPkgs = pkgs.llvmPackages_9;
   stdenv = llvmPkgs.stdenv;
-in
-stdenv.mkDerivation rec {
-  name = "afl";
+  afl = stdenv.mkDerivation rec {
+    name = "afl";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "google";
-    repo = "AFL";
-    rev = "master";
-    hash = "sha256-PU6DRRreZbLCZ/sNtR37uHMHqhcOxm+whRLzNFQT7Tw=";
+    src = pkgs.fetchFromGitHub {
+      owner = "google";
+      repo = "AFL";
+      rev = "master";
+      hash = "sha256-PU6DRRreZbLCZ/sNtR37uHMHqhcOxm+whRLzNFQT7Tw=";
+    };
+
+    # AFL implicitly relies on which to check for `llvm-config` availability.
+    buildInputs = [ llvmPkgs.llvm pkgs.which ];
+
+    installFlags = [ "PREFIX=$(out)" ];
+    postBuild = ''
+      make -C llvm_mode -j $NIX_BUILD_CORES
+    '';
   };
-
-  # AFL implicitly relies on which to check for `llvm-config` availability.
-  buildInputs = [ llvmPkgs.llvm pkgs.which ];
-
-  installFlags = [ "PREFIX=$(out)" ];
-  postBuild = ''
-    make -C llvm_mode -j $NIX_BUILD_CORES
+in
+afl // {
+  cc = "${afl}/bin/afl-clang-fast";
+  cxx = "${afl}/bin/afl-clang-fast++";
+  lib = ./src/afl_driver.cpp;
+  envConfigure = ''
+    export AFL_PATH=${afl}/lib/afl
   '';
+  inherit stdenv;
 }
--- targets/libpng/default.nix
+++ targets/libpng/default.nix
@@ -1,10 +1,10 @@
 { pkgs
 , llvmPkgs
 , stdenv
-, deps
+, fuzzer
 }:
 
-stdenv.mkDerivation {
+fuzzer.stdenv.mkDerivation {
   name = "libpng";
 
   src = pkgs.fetchFromGitHub {
@@ -14,24 +14,22 @@ stdenv.mkDerivation {
     hash = "sha256-KCpOY1kL4eG51bUv28aw8jTjUNwr3UHAGBqAaN2eBvg=";
   };
 
-  buildInputs = [ deps.zlib llvmPkgs.lld ];
+  buildInputs = [ pkgs.zlib ];
 
   dontDisableStatic = true;
-
   configureFlags = [
     "--disable-shared"
   ];
 
   preConfigure = ''
-    export CFLAGS="$CFLAGS -flto"
-    export CXXFLAGS="$CFlAGS"
-    export LDFLAGS="$LDFLAGS -fuse-ld=lld"
-  '';
+    export CC=${fuzzer.cc}
+    export CXX=${fuzzer.cxx}
+  '' + fuzzer.envConfigure;
 
   postInstall = ''
     echo "#include <stdlib.h>" > $out/libpng_read_fuzzer.cc
     cat $src/contrib/oss-fuzz/libpng_read_fuzzer.cc >> $out/libpng_read_fuzzer.cc
-    $CXX $CXXFLAGS -fsanitize=fuzzer -std=c++11 -I $out/include -o $out/bin/libpng_read_fuzzer -lz $LDFLAGS \
-      $out/lib/libpng16.a $out/libpng_read_fuzzer.cc
+    $CXX $CXXFLAGS -std=c++11 -I $out/include -o $out/bin/libpng_read_fuzzer -lz $LDFLAGS \
+      $out/libpng_read_fuzzer.cc $out/lib/libpng16.a ${fuzzer.lib}
   '';
 }
