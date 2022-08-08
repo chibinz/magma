6f37e33e3fdc61880ad62bc12a1dc378b067b89e
Chibin Zhang <chibinzhang@hotmail.com>
2022-08-08 20:27:18 +0000

    feat: Rewrite libtiff

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
         ];
         targets = [
           "libpng"
+          "libtiff"
           "libxml2"
           "lua"
           "openssl"
---
+++ targets/libtiff/default.nix
@@ -0,0 +1,44 @@
+{ stdenv
+, driver
+, fetchFromGitLab
+, autoconf
+, automake
+, libtool
+, libjpeg
+, lzma
+, zlib
+}:
+
+stdenv.mkDerivation {
+  name = "libtiff";
+
+  src = fetchFromGitLab {
+    owner = "libtiff";
+    repo = "libtiff";
+    rev = "c145a6c14978f73bb484c955eb9f84203efcb12e";
+    hash = "sha256-70cQGtLg0ILpO1/DpaDcqKW2vSI6V7nWisjxZWqwhuc=";
+  };
+
+  buildInputs = [ autoconf automake libtool libjpeg lzma zlib ];
+
+  enableParallelBuilding = true;
+
+  preConfigure = ''
+    # Prevents `autogen.sh` from fetching configs from the internet.
+    head -n 9 autogen.sh | bash
+    cp ${./src/config.guess} ${./src/config.sub} config/
+  '';
+
+  configureFlags = [ "--disable-shared" ];
+
+  postInstall = ''
+    c++ -std=c++11 -I $out/include -o $out/bin/tiff_read_rgba_fuzzer \
+        contrib/oss-fuzz/tiff_read_rgba_fuzzer.cc $out/lib/libtiffxx.a \
+        $out/lib/libtiff.a ${driver} -lz -ljpeg -llzma
+  '';
+
+  passthru = {
+    programs = [ "tiff_read_rgba_fuzzer" "tiffcp" ];
+    programArgs.tiffcp = "-M @@ tmp.out";
+  };
+}
