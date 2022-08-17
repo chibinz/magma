36e18752693a32be523a9d2263dffad451cf6e04
Chibin Zhang <chibinzhang@hotmail.com>
2022-08-17 03:57:17 +0000

    feat: improve `bench.sh`

--- bench.sh
+++ bench.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+set +x
 
 TARGETS=$(ls targets/)
 FUZZERS="afl aflplusplus"
 
-echo "Targets: $TARGETS"
-echo "Fuzzers: $FUZZERS"
-
-checkpoint() {
+clean() {
     # Prune all caches
     docker images -aq | xargs docker rmi -f
     nix store gc
+}
 
+checkpoint() {
     echo "*** Checkpoint ***"
     date
     docker images
@@ -18,13 +18,33 @@ checkpoint() {
     echo "******************"
 }
 
-checkpoint
+bench_orig() {
+    clean
+    checkpoint
 
-for fuzzer in $FUZZERS; do
-    for target in $TARGETS; do
-        echo "building $fuzzer-$target with build.sh"
-        env TARGET=$target FUZZER=$fuzzer tools/captain/build.sh
+    for fuzzer in $FUZZERS; do
+        for target in $TARGETS; do
+            echo "building $fuzzer-$target with build.sh"
+            env TARGET=$target FUZZER=$fuzzer tools/captain/build.sh
+        done
     done
-done
 
-checkpoint
+    checkpoint
+}
+
+
+bench_nix() {
+    clean
+    checkpoint
+
+    nix build --max-jobs $(nproc)
+    for im in result/*; do
+        docker load -i $im
+    done
+
+    checkpoint
+}
+
+bench_orig
+
+bench_nix
